f20af057ec1a70c99d8973f892ac4361fc38cc7b
Pierre <zins.pierre@gmail.com>
2017-05-10 10:51:37 -0400

    update

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -3,7 +3,7 @@ import sys
 import tensorflow as tf
 import time
 
-
+start_time = time.time()
 n = 2048
 dtype = tf.float32
 
@@ -20,3 +20,5 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 res = sess.run(product)
 print(res)
+elapsed_time = time.time() - start_time
+print("Elapsed Time :", elapsed_time)
