0b21e69a825680296cda516133dcfec76908ca3f
Pierre <zins.pierre@gmail.com>
2017-05-12 10:13:56 -0400

    update

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
-python bookmarks.py ~/.config/chromium/Default/Bookmarks bookmarks.json
+#!/usr/bin/sh
+
+python bookmarks.py ~/.config/chromium/Default/Bookmarks bookmarks.html
 git add .
 git add -u
 git commit -am "update"
