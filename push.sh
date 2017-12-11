7518cb5a876cca3ef8a48d2dcb9b32cbcbe285cc
Pierre <zins.pierre@gmail.com>
2017-12-11 11:07:39 -0500

    change script for the new ubuntu

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-python bookmarks.py ~/.config/chromium/Default/Bookmarks bookmarks.html
+python bookmarks.py ~/.config/chromium/Default/Bookmarks bookmarks_ubuntu_hiptf.html
 git add .
 git add -u
 git commit -am "update"
