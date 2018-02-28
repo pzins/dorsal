7db6c49235fa3720bd173921f728a1c7f174c6fa
Pierre <zins.pierre@gmail.com>
2018-02-28 17:03:16 -0500

    fix wrong lttng tracepoint name

--- tracing_scripts/trace_tensorflow_grpc.sh
+++ tracing_scripts/trace_tensorflow_grpc.sh
@@ -5,6 +5,6 @@ sudo lttng enable-event --userspace "hipTracer:*"
 sudo lttng enable-event --userspace "tensorflowTracer:*"
 sudo lttng enable-event --userspace "grpcTracer:*"
 sudo lttng enable-event -k net_dev_queue
-sudo lttng enable-event -k netif_receive_skb
+sudo lttng enable-event -k net_if_receive_skb
 sudo lttng add-context -u -t vtid
 sudo lttng start
