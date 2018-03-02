ce0bb45cf7d72c5afbb915af32ccb5e562f03834
Pierre <zins.pierre@gmail.com>
2018-03-02 18:09:15 -0500

    Changes in sort script + improve my callstack view
    Allow several events with the same timestamp in ths sorting script.
    Add information in the hcc kernels and copy events (workgroup size,
    grid size, ...). And Improve my callstack view so that, the result
    is split by threadId. This allows to trace several Tensorflow
    programm at the same time.

--- tracing_scripts/sort_events.py
+++ tracing_scripts/sort_events.py
@@ -7,6 +7,8 @@ from tracing_events_classes import event_classes
 from collections import defaultdict
 import time
 import os
+from collections import defaultdict
+
 
 
 # Add the input trace to the collection
@@ -40,11 +42,13 @@ for event_class in event_classes.values():
     main_stream_class.add_event_class(event_class)
 main_stream = writer.create_stream(main_stream_class)
 
+cntol = 0
+events = defaultdict(list)
 
-events = {}
 # clock_offset = 1518196357777395130 # second computer
-clock_offset = 1519157918746548550 # first computer
-
+clock_offset = 1519939145097366944 # first computer
+save_barrier_time = 0
+cnt_incoherent_barrier = 0
 for r_event in collection.events:
     name = r_event.name
     event_time = r_event.timestamp
@@ -55,22 +59,42 @@ for r_event in collection.events:
 
     for f in fields:
         # print(name, f, r_event[f])
+        # if hccTracer:kernel_* : fill the grid and groupworker arrays
+        if f == "workgroup_size" or f == "grid_size":
+            for i in range(3):
+                tmp = w_event.payload(f).field(i)
+                tmp.value = r_event[f][i]
+            continue
+            
         w_event.payload(f).value = r_event[f]
 
     if "hccTracer:kernel" in name or "hccTracer:async" in name or "hccTracer:barrier" in name:
         event_time = r_event["timestamp"] + clock_offset
+        if save_barrier_time == 0:
+            save_barrier_time = r_event["timestamp"]
+        if "barrier" in name:
+            # if time between last barrier and this barrier time (start or end) we skip it
+            if abs(r_event["timestamp"] - save_barrier_time) > 1000000000 * 120:
+                print("barrier incoherent time")
+                cnt_incoherent_barrier += 1
+                continue
+            save_barrier_time = r_event["timestamp"]
 
 
 
     # organize threads
     threadId = r_event.field_with_scope("vtid", babeltrace.common.CTFScope.STREAM_EVENT_CONTEXT)
-    if "RecvTensor" in name:
-        threadId = 1111
-    elif "grpc" in name:
-        continue
+    # if "RecvTensor" in name:
+    #     threadId = 1111
+    # elif "grpc" in name:
+    #     continue
     # do not change vtid
     # events[event_time-1519157918746548549] = [w_event, threadId]
-    events[event_time] = [w_event, threadId]
+    if event_time in events:
+        print("timestamp already exists")
+        cntol += 1
+
+    events[event_time].append([w_event, threadId])
     continue
 
     if "tensorflowTracer:session" in name or "tensorflowTracer:process" in name or "tensorflowTracer:inline_ready" in name or "tensorflowTracer:push_succ" in name:
@@ -124,16 +148,17 @@ for r_event in collection.events:
         threadId = 99999
 
     events[event_time] = [w_event, threadId]
-
 # Append events to the stream
 timestamps = list(events.keys())
 timestamps.sort()
 
 for timestamp in timestamps:
     clock.time = timestamp
-    ev = events[timestamp][0]
-    ev.tid(events[timestamp][1])
-    main_stream.append_event(ev)
+    for i in range(len(events[timestamp])):
+        ev = events[timestamp][i][0]
+        ev.tid(events[timestamp][i][1])
+        # print(timestamp)
+        main_stream.append_event(ev)
 
 # Flush the stream
 main_stream.flush()
