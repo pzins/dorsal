7cb4a065a03a249d83522349df36ccebf5ab19c2
Pierre Zins <zins.pierre@gmail.com>
2018-04-06 16:27:58 -0400

    sorting script with hc log output
    sorting script which integrate hc log output to the trace
    fix bug in event class

--- tracing_scripts/trace_tensorflow.sh
+++ tracing_scripts/trace_tensorflow.sh
@@ -1,9 +1,21 @@
 lttng create tensorflow
 lttng enable-event --userspace "hsa_runtime:*"
 lttng enable-event --userspace "hsaTracer:*"
-lttng enable-event --userspace "hccTracer:*"
+# lttng enable-event --userspace "hccTracer:*"
 lttng enable-event --userspace "hipTracer:*"
 lttng enable-event --userspace "tensorflowTracer:*"
 lttng enable-event --userspace "streamTracer:*"
 lttng add-context -u -t vtid
 lttng start
+
+
+# sudo lttng create tensorflow
+# sudo lttng enable-event -k -a
+# sudo lttng enable-event --userspace "hsa_runtime:*"
+# sudo lttng enable-event --userspace "hsaTracer:*"
+# sudo lttng enable-event --userspace "hccTracer:*"
+# sudo lttng enable-event --userspace "hipTracer:*"
+# sudo lttng enable-event --userspace "tensorflowTracer:*"
+# sudo lttng enable-event --userspace "streamTracer:*"
+# sudo lttng add-context -u -t vtid
+# sudo lttng start
