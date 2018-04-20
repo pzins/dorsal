9c00807d3a223917f1fa36fa3cc222bf37ce7ae7
Pierre Zins <zins.pierre@gmail.com>
2018-04-20 12:08:15 -0400

    update fabric script

--- tracing_scripts/grpc_scripts/fabfile.py
+++ tracing_scripts/grpc_scripts/fabfile.py
@@ -3,8 +3,14 @@ from fabric.api import *
 from fabric.contrib.console import confirm
 from fabric.context_managers import cd, env
 
-_HOSTS = [ "192.168.1.5", "192.168.1.3" ] 
-env.hosts = "192.168.1.5"
+computer_1_ip = "192.168.1.3"
+computer_1_ip = "132.207.72.22"
+computer_2_ip = "192.168.1.5"
+computer_2_ip = "132.207.72.31"
+tf_program_name = "cnn_distributed"
+
+_HOSTS = [ computer_1_ip, computer_2_ip ] 
+env.hosts = computer_1_ip
 env.shell = "/usr/bin/fish -l -i -c"
 
 @task
@@ -16,28 +22,27 @@ def startTracing():
 def runProgramTF():
     path_to_tf_script = "/home/pierre/Dropbox/dev/distributed/in_model_parallelism/"
     with cd(path_to_tf_script):
-        if env.host == "192.168.1.5":
+        if env.host == computer_1_ip:
             with settings(hide('warnings'), warn_only=True):
-                run("set -x HIP_PROFILE_API 2; set -x HCC_PROFILE 2; python3 cnn_distributed.py w > /dev/null 2>&1")
+                run("set -x HIP_PROFILE_API 2; set -x HCC_PROFILE 2; python3 " + tf_program_name + ".py w > /dev/null 2>&1")
         else:
-            run("set -x HIP_PROFILE_API 2; set -x HCC_PROFILE 2; python3 cnn_distributed.py m  > /dev/null 2>&1")
-            with settings(host_string='192.168.1.5'):
-                prog_name=  "cnn_distributed"
-                run("kill -SIGKILL (ps -aux | grep " + prog_name + " | grep -v grep | awk '{print $2}')")
+            run("set -x HIP_PROFILE_API 2; set -x HCC_PROFILE 2; python3 " + tf_program_name + ".py m  > /dev/null 2>&1")
+            with settings(host_string=computer_2_ip):
+                run("kill -SIGKILL (ps -aux | grep " + tf_program_name + " | grep -v grep | awk '{print $2}')")
 
 @task
 @parallel
 def stopTracing():
-    if env.host == "192.168.1.3":
+    if env.host == computer_2_ip:
         run("sudo lttng destroy; sudo chown -R pierre:pierre ~/lttng-traces/")
         run("python3 ~/sort_events.py")
         run("python3 ~/vtid.py")
-    elif env.host == "192.168.1.5":
+    elif env.host == computer_1_ip:
         run("sudo lttng destroy; sudo chown -R pierre:pierre ~/lttng-traces/")
         run("python3 ~/sort_events_second.py")
         run("python3 ~/vtid_second.py")
-        run("scp -r ~/remote_traces 192.168.1.3:~/")
-        run("scp -r ~/lttng-traces/(ls -t lttng-traces/ | head -n1) 192.168.1.3:~/")
+        run("scp -r ~/remote_traces " + computer_1_ip + ":~/")
+        run("scp -r ~/lttng-traces/(ls -t lttng-traces/ | head -n1) " + computer_1_ip + ":~/")
 
 @task
 def main():
